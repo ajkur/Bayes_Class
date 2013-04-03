444c8576e73b1d50feaa88386d5a2c7c6ba4ca16
ajkur <ajkurzawski@gmail.com>
2013-04-03 03:04:30 -0500

    HW4: model changes, add to writeup.

--- hw4/Scripts/Prob2/models.py
+++ hw4/Scripts/Prob2/models.py
@@ -12,16 +12,34 @@ def mod_1():
 
     # Priors
     tau_sq = pymc.Uniform('tau_sq', lower=0., upper=1., value=0.5)
+    # tau_sq = pymc.Beta('tau_sq', alpha=1, beta=6, value=0.01)
     theta = pymc.Normal('theta', mu = 0, tau=10**-6, value=0)
 
     # Second Level
-    theta_j = pymc.Normal('theta_j', mu = theta, tau=tau_sq**0.5, value=0)
+    theta_j = pymc.Normal('theta_j', mu = [theta]*22, tau=[1/tau_sq]*22, value=[0]*22)
 
     # Likelihood function
     y_j_obs = pymc.Normal('y_j_obs', value=y_j, mu=theta_j, tau=1/V_j, observed=True)
 
     return vars()
 
+def mod_2():
+    # Data
+    y0, n0, y1, n1 = data.full()
+    y_j = np.log(y0/(n0 - y0)) - np.log(y1/(n1 - y1))
+    V_j = 1.0/y1 + 1.0/(n1 - y1) + 1.0/y0 + 1.0/(n0 - y1)
+
+    # Priors
+    tau_sq = pymc.Beta('tau_sq', alpha=1, beta=6, value=0.01)
+    theta = pymc.Normal('theta', mu = 0, tau=10**-6, value=0)
+
+    # Second Level
+    theta_j = pymc.Normal('theta_j', mu = [theta]*22, tau=[1/tau_sq]*22, value=[0]*22)
+
+    # Likelihood function
+    y_j_obs = pymc.Binomial('y_j_obs', value=y_j, p=theta_j, n=n_j, observed=True)
+
+    return vars()
 
 
 
--- hw4/Scripts/Prob2/sampler.py
+++ hw4/Scripts/Prob2/sampler.py
@@ -5,15 +5,20 @@ import numpy as np
 import data
 
 # y0, n0, y1, n1 = data.full()
-# print y0/(n0 - y0)
+# y_j = np.log(y0/(n0 - y0)) - np.log(y1/(n1 - y1))
+# V_j = 1.0/y1 + 1.0/(n1 - y1) + 1.0/y0 + 1.0/(n0 - y1)
+# print y_j
+
 
 mod = 1
 
 if mod == 1:
     vars = models.mod_1()
     m = mc.MCMC(vars)
-    m.use_step_method(mc.AdaptiveMetropolis, [m.theta,m.theta_j])
-    m.sample(iter=10000, burn=0, thin=1)
+    # m.use_step_method(mc.AdaptiveMetropolis, [m.theta,m.theta_j,m.tau_sq])
+    m.sample(iter=100000, burn=50000, thin=70)
+    # m.sample(iter=10000, burn=0, thin=1)
+    
 if mod == 2:
     vars = models.mod_2()
     m = mc.MCMC(vars)
