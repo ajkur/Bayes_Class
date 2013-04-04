37eff729d5abcd0dc9b524232df1012f71a6fe94
ajkur <ajkurzawski@gmail.com>
2013-04-03 19:36:37 -0500

    HW4: almost.

--- hw4/Scripts/Prob2/models.py
+++ hw4/Scripts/Prob2/models.py
@@ -9,10 +9,11 @@ def mod_1():
     y0, n0, y1, n1 = data.full()
     y_j = np.log(y0/(n0 - y0)) - np.log(y1/(n1 - y1))
     V_j = 1.0/y1 + 1.0/(n1 - y1) + 1.0/y0 + 1.0/(n0 - y1)
+    sig2_c = sum(V_j)
 
     # Priors
-    tau_sq = pymc.Uniform('tau_sq', lower=0., upper=1., value=0.5)
-    # tau_sq = pymc.Beta('tau_sq', alpha=1, beta=6, value=0.01)
+    # tau_sq = pymc.Uniform('tau_sq', lower=0., upper=1., value=0.5)
+    tau_sq = pymc.Beta('tau_sq', alpha=1, beta=6, value=0.01)
     theta = pymc.Normal('theta', mu = 0, tau=10**-6, value=0)
 
     # Second Level
@@ -26,18 +27,31 @@ def mod_1():
 def mod_2():
     # Data
     y0, n0, y1, n1 = data.full()
-    y_j = np.log(y0/(n0 - y0)) - np.log(y1/(n1 - y1))
-    V_j = 1.0/y1 + 1.0/(n1 - y1) + 1.0/y0 + 1.0/(n0 - y1)
+    # y_j = np.log(y0/(n0 - y0)) - np.log(y1/(n1 - y1))
+    # V_j = 1.0/y1 + 1.0/(n1 - y1) + 1.0/y0 + 1.0/(n0 - y1)
 
     # Priors
+    # tau_sq = pymc.Uniform('tau_sq', lower=0., upper=1., value=0.5)
     tau_sq = pymc.Beta('tau_sq', alpha=1, beta=6, value=0.01)
     theta = pymc.Normal('theta', mu = 0, tau=10**-6, value=0)
+    p1p = pymc.Uniform('p1p', lower=0., upper=1., value=0.5)
+    p0p = pymc.Uniform('p0p', lower=0., upper=1., value=0.5)
 
     # Second Level
     theta_j = pymc.Normal('theta_j', mu = [theta]*22, tau=[1/tau_sq]*22, value=[0]*22)
 
+    @deterministic
+    def p0(th=theta_j,p1=p1p):
+        c1 = np.exp(-th) + p1/(1+p1)
+        return c1/(1+c1)
+    @deterministic
+    def p1(th=theta_j,p0=p0p):
+        c0 = np.exp(-th) + p0/(1+p0)
+        return c0/(1+c0)
+
     # Likelihood function
-    y_j_obs = pymc.Binomial('y_j_obs', value=y_j, p=theta_j, n=n_j, observed=True)
+    y_j_obs_1 = pymc.Binomial('y_j_obs_1', value=y1, p=p1, n=n1, observed=True)
+    y_j_obs_0 = pymc.Binomial('y_j_obs_0', value=y0, p=p0, n=n0, observed=True)
 
     return vars()
 
--- hw4/Scripts/Prob2/sampler.py
+++ hw4/Scripts/Prob2/sampler.py
@@ -7,23 +7,32 @@ import data
 # y0, n0, y1, n1 = data.full()
 # y_j = np.log(y0/(n0 - y0)) - np.log(y1/(n1 - y1))
 # V_j = 1.0/y1 + 1.0/(n1 - y1) + 1.0/y0 + 1.0/(n0 - y1)
-# print y_j
+# # print sum(V_j)
+# # print np.mean(y_j)
+# for i in range(y_j.shape[0]):
+#     # print y_j[i]
+#     print y1[i]/n1[i]
+# p0=np.array([0.081, 0.089, 0.093, 0.075, 0.08 , 0.092, 0.132, 0.097, 0.109, 0.085, 0.08 , 0.168, 0.049, 0.06 , 0.179, 0.162, 0.107, 0.057, 0.042, 0.16 , 0.093, 0.048])
+# p1=np.array([0.081, 0.089, 0.093, 0.075, 0.08 , 0.092, 0.132, 0.097, 0.109, 0.085, 0.08 , 0.168, 0.049, 0.06 , 0.179, 0.162, 0.107, 0.057, 0.042, 0.16 , 0.093, 0.048])
+
+# for i in range(p0.shape[0]):
+#     print np.log(p0[i]/(1 - p0[i])) #- np.log(p1[i]/(1 - p1[i]))
 
 
-mod = 1
+
+
+mod = 2
 
 if mod == 1:
     vars = models.mod_1()
     m = mc.MCMC(vars)
-    # m.use_step_method(mc.AdaptiveMetropolis, [m.theta,m.theta_j,m.tau_sq])
-    m.sample(iter=100000, burn=50000, thin=70)
-    # m.sample(iter=10000, burn=0, thin=1)
+    m.sample(iter=100000, burn=70000, thin=70)
     
 if mod == 2:
     vars = models.mod_2()
     m = mc.MCMC(vars)
-    m.use_step_method(mc.AdaptiveMetropolis, [m.tau,m.theta])
-    m.sample(iter=10000, burn=0, thin=1)
+    m.use_step_method(mc.AdaptiveMetropolis, [m.theta])
+    m.sample(iter=20000, burn=10000, thin=10)
 
 print m.summary()
 print 'DIC=%f' % m.dic
